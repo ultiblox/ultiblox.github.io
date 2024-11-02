d772825f54e4536a89410c1b79e42faa834e7e51
Maker Zone <dev@makerzone.com.au>
2024-11-02 14:59:19 +1100

    Fixing build

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Manually run Tailwind CSS build
-npx tailwindcss -i ./src/assets/css/main.css -o ./dist/assets/css/main.css --minify
-
 # Run the full build with npm build
 npm run build
