254bec9f5a78298f016e4ed0a1b6e2edb66b9eca
Maker Zone <dev@makerzone.com.au>
2024-11-02 16:17:00 +1100

    Renaming ultiblocks to ultiblox

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 // src/_data/site.js
 module.exports = {
-  title: 'UltiBlocks',
-  description: 'UltiBlocks is a flexible ecosystem of modular electronics enclosures and Arduino libraries, designed for makers, educators, and engineers. Mix and match components to build custom solutions with ease, powered by robust, reusable code and adaptable hardware.',
-  keywords: ['modular', 'electronics', 'Arduino', 'maker', 'Ultiblocks'],
+  title: 'UltiBlox',
+  description: 'UltiBlox is a flexible ecosystem of modular electronics enclosures and Arduino libraries, designed for makers, educators, and engineers. Mix and match components to build custom solutions with ease, powered by robust, reusable code and adaptable hardware.',
+  keywords: ['modular', 'electronics', 'Arduino', 'maker', 'UltiBlox'],
   url: 'https://ultiblox.org', // your site URL without trailing slash
   paginate: 6,
   cover_image: '/assets/img/banner.webp'
